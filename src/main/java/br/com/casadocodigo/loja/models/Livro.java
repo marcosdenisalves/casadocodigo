7704a8a1dbb5665e829599c13d0de830644eac3c
marcosdenisalves <marcos.denisjr@gmail.com>
2020-12-09 21:58:07 -0300

    Adicionando lista de autores

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -1,12 +1,15 @@
 package br.com.casadocodigo.loja.models;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,40 +19,61 @@ public class Livro {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String titulo;
 	@Lob
 	private String descricao;
 	private BigDecimal preco;
 	private Integer numeroPaginas;
-	
+
+	@ManyToMany
+	private List<Autor> autores = new ArrayList<>();
+
+	public Livro() {}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public BigDecimal getPreco() {
 		return preco;
 	}
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
+
 	public Integer getNumeroPaginas() {
 		return numeroPaginas;
 	}
+
 	public void setNumeroPaginas(Integer numeroPaginas) {
 		this.numeroPaginas = numeroPaginas;
 	}
+
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
+	}
+
 	@Override
 	public String toString() {
-		return "Livro [titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco + ", numeroPaginas="
-				+ numeroPaginas + "]";
+		return "Livro [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco
+				+ ", numeroPaginas=" + numeroPaginas + ", autores=" + autores + "]";
 	}
- }
+}
