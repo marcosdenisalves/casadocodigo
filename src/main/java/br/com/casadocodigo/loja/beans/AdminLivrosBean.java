7d8cfcb8a0ad56ea975226e5ddf390ac88fdfb6f
marcosdenisalves <marcos.denisjr@gmail.com>
2020-12-09 21:56:06 -0300

    Criando AdminLivrosBean

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,27 +1,42 @@
 package br.com.casadocodigo.loja.beans;
 
-import javax.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.inject.Model;
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.transaction.Transactional;
 
+import br.com.casadocodigo.loja.daos.AutorDao;
 import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Autor;
 import br.com.casadocodigo.loja.models.Livro;
 
-@Named
-@RequestScoped
+@Model
 public class AdminLivrosBean {
-	
+
 	@Inject
 	private Livro livro;
-	
 	@Inject
-	private LivroDao dao;
+	private LivroDao livroDao;
+	@Inject
+	private AutorDao autorDao;
 	
+	private List<Integer> autoresId = new ArrayList<>();
+
 	@Transactional
-	public void salvar() {
+	public String salvar() {
+		for (Integer autorId: autoresId) {
+			livro.getAutores().add(new Autor(autorId));
+		}
+		livroDao.salvar(livro);
 		System.out.println("Livro Cadastrado: " + livro);
-		dao.salvar(livro);
+		
+		return "/livros/lista?faces-redirect=true";
+	}
+
+	public List<Autor> getAutores() {
+		return autorDao.listaTodos();
 	}
 
 	public Livro getLivro() {
@@ -30,5 +45,13 @@ public class AdminLivrosBean {
 
 	public void setLivro(Livro livro) {
 		this.livro = livro;
+	}
+
+	public List<Integer> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Integer> autoresId) {
+		this.autoresId = autoresId;
 	}
 }
