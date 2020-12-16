a31834d8a6496d3d643455068b32601b64d990c2
marcosdenisalves <marcos.denisjr@gmail.com>
2020-12-16 20:25:59 -0300

    Adicionando métodos lista livros

--- src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -20,4 +20,14 @@ public class LivroDao {
 		String jpql = "select distinct(l) from Livro l join fetch l.autores";
 		return manager.createQuery(jpql, Livro.class).getResultList();
 	}
+
+	public List<Livro> ultimosLancamentos() {
+		String jpql = "select l from Livro l order by l.id desc";
+		return manager.createQuery(jpql, Livro.class).setMaxResults(5).getResultList();
+	}
+
+	public List<Livro> demaisLivros() {
+		String jpql = "select l from Livro l order by l.id desc";
+		return manager.createQuery(jpql, Livro.class).setFirstResult(6).getResultList();
+	}
 }
