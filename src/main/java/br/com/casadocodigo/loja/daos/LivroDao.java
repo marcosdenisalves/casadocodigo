93947c24faadbdb37a2e3e79eee74295d15c5cfa
marcosdenisalves <marcos.denisjr@gmail.com>
2020-12-16 21:44:43 -0300

    adicionando método busca por id

--- src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -28,6 +28,11 @@ public class LivroDao {
 
 	public List<Livro> demaisLivros() {
 		String jpql = "select l from Livro l order by l.id desc";
-		return manager.createQuery(jpql, Livro.class).setFirstResult(6).getResultList();
+		return manager.createQuery(jpql, Livro.class).setFirstResult(5).getResultList();
+	}
+
+	public Livro buscarPorId(Integer id) {
+		String jpql = "select l from Livro l join fetch l.autores where l.id = :id";
+		return manager.createQuery(jpql, Livro.class).setParameter("id", id).getSingleResult();
 	}
 }
