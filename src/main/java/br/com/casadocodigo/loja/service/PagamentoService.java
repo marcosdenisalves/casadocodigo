6f70917cf5dde9d2637f6c31a4326f2c46c7457f
marcosdenisalves <marcos.denisjr@gmail.com>
2021-01-09 18:40:54 -0300

    Executando serviços assincronos

--- src/main/java/br/com/casadocodigo/loja/service/PagamentoService.java
+++ src/main/java/br/com/casadocodigo/loja/service/PagamentoService.java
@@ -1,12 +1,17 @@
 package br.com.casadocodigo.loja.service;
 
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
@@ -14,26 +19,33 @@ import javax.ws.rs.core.UriBuilder;
 import br.com.casadocodigo.loja.daos.CompraDao;
 import br.com.casadocodigo.loja.models.Compra;
 
-
 @Path("/pagamento")
 public class PagamentoService {
-	
+
 	@Inject
 	private CompraDao dao;
 	@Context
 	private ServletContext context;
-	
+
+	private static ExecutorService executor = Executors.newFixedThreadPool(50);
+
 	@POST
-	public Response pagar(@QueryParam("uuid") String uuid) {
+	public void pagar(@Suspended final AsyncResponse ar, @QueryParam("uuid") String uuid) {
 		Compra compra = dao.buscaPorUuid(uuid);
-		PagamentoGateway.pagar(compra.getTotal());
-		
-		URI responseUri = UriBuilder.fromPath("http://localhost:8080" + 
-				context.getContextPath()+"/index.xhtml")
-			.queryParam("msg", "Compra Realizada com Sucesso")
-			.build();
-		Response response = Response.seeOther(responseUri).build();
- 		
-		return response;
+
+		executor.submit(() -> {
+			try {
+				PagamentoGateway.pagar(compra.getTotal());
+
+				URI responseUri = UriBuilder
+						.fromPath("http://localhost:8080" + context.getContextPath() + "/index.xhtml")
+						.queryParam("msg", "Compra Realizada com Sucesso").build();
+
+				ar.resume(Response.seeOther(responseUri).build());
+
+			} catch (Exception e) {
+				ar.resume(new WebApplicationException(e));
+			}
+		});
 	}
 }
